291ecd4edf0b30ad073e6343105bd01e3b2add92
Hafthor <git@hafthor.net>
2024-08-31 23:06:12 -0400

    slight change to display

--- JokersAndMarbles/Game.cs
+++ JokersAndMarbles/Game.cs
@@ -16,18 +16,17 @@ public class Game {
             if (sCmd == "") {
                 sCmd = board.AutoPlay();
                 board.Print($"Auto{board.Turn}: {sCmd}");
-            }
+            } else
+                board.Print($"P{board.Turn}: {sCmd}");
             string s = board.Play(sCmd);
             if (s != null) {
                 board.Print(s);
-            } else {
-                if (board.Win) {
-                    board.Paint();
-                    Console.WriteLine($"Players {board.Turn} and {board.Teammate} win!");
-                    break;
-                }
+            } else if (board.Win) {
+                board.Print($"Game Over - Players {board.Turn} and {board.Teammate} win!");
+                board.Paint();
+                break;
+            } else
                 board.NextTurn();
-            }
         }
     }
 }
