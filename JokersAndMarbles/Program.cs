972040c67c9f5d79820e40518c128ff2b313c691
Hafthor <git@hafthor.net>
2024-09-02 11:04:04 -0400

    reset defaults

--- JokersAndMarbles/Program.cs
+++ JokersAndMarbles/Program.cs
@@ -2,8 +2,8 @@
 
 public class Program {
     public static void Main(string[] args) {
-        int players = args.Length > 0 ? int.Parse(args[0]) : 8;
-        Random random = args.Length > 1 ? new(int.Parse(args[1])) : new(0);
+        int players = args.Length > 0 ? int.Parse(args[0]) : 4;
+        Random random = args.Length > 1 ? new(int.Parse(args[1])) : new();
         Game game = new(players, random);
         game.Run();
     }
