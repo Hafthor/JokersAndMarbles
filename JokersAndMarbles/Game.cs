75684309e8c048b30524260f2c4329b658e7685b
Hafthor <git@hafthor.net>
2024-09-04 19:08:50 -0400

    nits

--- JokersAndMarbles/Game.cs
+++ JokersAndMarbles/Game.cs
@@ -9,14 +9,14 @@ public class Game(int playerCount, Random rnd) {
             string clr = Marble.ColorForPlayer(_board.Turn, playerCount);
             Console.Write($"{clr}Player {_board.Turn}[{_board.Score()}]{Ansi.Reset} ({_board.Hand()}):");
             string sCmd = Console.ReadLine() ?? "";
-            if (sCmd == "exit" || sCmd == "quit") break;
+            if (sCmd is "exit" or "quit") break;
             if (sCmd == "plays") {
                 var plays = _board.LegalPlays().OrderByDescending(p => p.score);
                 Console.WriteLine(string.Join(", ", plays.Select(p => $"{p.play}={p.score}")));
                 Console.ReadLine();
                 continue;
             }
-            if (sCmd == "" || sCmd == "auto") sCmd = _board.LegalPlays().MaxBy(p => p.score).play;
+            if (sCmd is "" or "auto") sCmd = _board.LegalPlays().MaxBy(p => p.score).play;
             string s = _board.Play(sCmd);
             if (s != null)
                 _board.Print(s);
